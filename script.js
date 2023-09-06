9feecc33e6ec24218995013f108b509c1fedf449
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-09-07 01:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let level=document.getElementById('level');
+let domlevel=0;
+ while(level.parentNode){
+	level=level.parentNode;
+	domlevel++;
+}
+alert(domlevel);
